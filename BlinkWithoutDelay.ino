10722814fe3a5e4cc00a035ad4e1176b43237f51
tdalejandro01 <tic-320119@utnay.edu.mx>
2025-10-04 22:15:05 -0700

    Diego Duran Aport

--- BlinkWithoutDelay.ino
+++ BlinkWithoutDelay.ino
@@ -49,3 +49,30 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
 }
+
+oid loop() {
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+    digitalWrite(PIN_SPI_MISO, ledState);  // turn the LED off by making the voltage LOW
+    digitalWrite(PIN_SPI_MOSI, ledState);  // turn the LED off by making the voltage LOW
+    digitalWrite(PIN_SPI_SS, ledState);
+  }
+}
