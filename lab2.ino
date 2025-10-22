c9051b82bad1123cff69ea8623dd2c049a3210aa
kevingodoy0 <tic-320128@utnay.edu.mx>
2025-10-21 18:28:52 -0700

    Kevin Imanol Godoy Romo - Aporte

--- lab2.ino
+++ lab2.ino
@@ -1,1 +1,12 @@
+/ Ejemplo: AnalogReadSerial adaptado para sensor LM35
+// Lee A0, muestra ADC, voltaje y temperatura en grados Celsius.
 
+const int pinLM35 = A0; // Pin analógico donde está conectado el sensor LM35
+
+const int ledVerde = 13;
+const int ledAmarillo = 12;
+const int ledRojo = 11;
+
+float temperatura;
+
+void setup() {
