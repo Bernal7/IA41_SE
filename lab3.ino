3845b33a4ff7164d92df9459daab87240ba3613d
kevingodoy0 <tic-320128@utnay.edu.mx>
2025-10-30 20:39:54 -0700

    Kevin Imanol Godoy Romo APORTEEE

--- lab3.ino
+++ lab3.ino
@@ -5,3 +5,13 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(3, INPUT);
 }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int pullDetection = digitalRead(3);
+  int buttonState = digitalRead(2);
+  Serial.print("Pull ?: ");
+  Serial.print(pullDetection);
+  Serial.print(" Boton Estado? :");
+  Serial.print(pullDetection);
+  Serial.print("\n");
