2c3bb89b90c9e4d1e007e464d9c271e4cc13cd32
#Fernangel <105268105+Fernangel7@users.noreply.github.com>
2025-10-30 22:51:56 -0700

    Update lab3.ino

--- lab3.ino
+++ lab3.ino
@@ -1,45 +1,95 @@
+const int buttonPin = 2;
+const int buttonPin2 = 3;
+const int ledPin = 13;
+
+int ledState = HIGH;
+int buttonState;
+int buttonState2;
+int lastButtonState = LOW;
+int lastButtonState2 = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+unsigned long lastDebounceTime2 = 0;
+unsigned long debounceDelay2 = 50;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin2, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  digitalWrite(ledPin, ledState);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int pullDetection = digitalRead(3);
-  int buttonState = digitalRead(2);
-  Serial.print("Pull ?: ");
-  Serial.print(pullDetection);
-  Serial.print(" Boton Estado? :");
-  Serial.print(pullDetection);
-  Serial.print("\n");
+  int reading = digitalRead(buttonPin);
+  int reading2 = digitalRead(buttonPin2);
+  Serial.println(reading);
 
-switch (pullDetection + 1) {
-    case 1: {
-      //pull up state
-      if (buttonState == HIGH) {
-        // turn LED on:
-        digitalWrite(13, HIGH);
-      } else {
-        // turn LED off:
-        digitalWrite(13, LOW);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    
+    if (reading != buttonState) {
+      buttonState = reading;
+      
+            switch (buttonState2) {
+        case 0: {
+          if (buttonState == HIGH) {
+            ledState = HIGH;
+          } else {
+            ledState = LOW;
+          }
+          break;
+        }
+        case 1: {
+          if (buttonState == LOW) {
+            ledState = HIGH;
+          } else {
+            ledState = LOW;
+          }
+          break;
+        }
       }
-      break;
-    }
-    case 2: {
-      //pull down state
-      if (buttonState == LOW) {
-        // turn LED on:
-        digitalWrite(13, HIGH);
-      } else {
-        // turn LED off:
-        digitalWrite(13, LOW);
-      }
-      break;
     }
   }
+  
+  if (reading2 != lastButtonState2) {
+    lastDebounceTime2 = millis();
+  }
+  
+  if ((millis() - lastDebounceTime2) > debounceDelay2) {
 
-  delay(500);
+    if (reading2 != buttonState2) {
+      buttonState2 = reading2;
+      
+            switch (buttonState2) {
+        case 0: {
+          if (buttonState == HIGH) {
+            ledState = HIGH;
+          } else {
+            ledState = LOW;
+          }
+          break;
+        }
+        case 1: {
+          if (buttonState == LOW) {
+            ledState = HIGH;
+          } else {
+            ledState = LOW;
+          }
+          break;
+        }
+      }
+    }
+  }
+  
+  digitalWrite(ledPin, ledState);
+
+  lastButtonState = reading;
+  lastButtonState2 = reading2;
 }
