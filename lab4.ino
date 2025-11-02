dfd9c575075e903b5a4e99b01ee2be400a0d1246
#Fernangel <105268105+Fernangel7@users.noreply.github.com>
2025-11-01 20:28:35 -0700

    Update lab4.ino

--- lab4.ino
+++ lab4.ino
@@ -1,1 +1,19 @@
+#include "DHT.h"
 
+#define DHTPIN 2
+
+// Pin donde está conectado el DHT11
+
+#define DHTTYPE DHT11
+
+//Especificamos el tipo de sensor
+
+int btnPin = 3;
+
+int ledRojoPin = 12;
+int ledAmarilloPin = 11;
+int ledVerdePin = 10;
+
+int pushed = 0;
+
+DHT dht(DHTPIN, DHTTYPE);
