7be671ecff064e35f1ef8835790b35dae6259030
#Fernangel <105268105+Fernangel7@users.noreply.github.com>
2025-11-08 20:42:36 -0700

    Add ESP32 HTTP request example with LED control
    This code connects an ESP32 to a WiFi network and makes an HTTP GET request to a specified URL. It controls an LED based on the HTTP response status.

---
+++ test/http_request.ino
@@ -0,0 +1,77 @@
+/*
+  * ESP32 + LED + 220 OHMS Resistor
+*/
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "MEGACABLE-2.4G-77A5";
+const char* password = "dB44s6td42";
+const int ledPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+
+  Serial.println("\nConectando al WiFi...");
+  WiFi.begin(ssid, password);
+
+  int intento = 0;
+  while (WiFi.status() != WL_CONNECTED && intento < 20) {
+    delay(1000);
+    Serial.print(".");
+    intento++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("\n✅ Conectado!");
+    Serial.print("IP asignada: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("\n❌ No se pudo conectar al WiFi");
+  }
+
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = "https://google.com/"; // Tu URL original
+
+    http.begin(url);
+    int httpCode = http.GET();
+
+    if (httpCode > 0) {
+      Serial.printf("Código HTTP: %d\n", httpCode);
+
+      // Si el servidor devuelve redirección (301 o 302)
+      if (httpCode == HTTP_CODE_MOVED_PERMANENTLY || httpCode == HTTP_CODE_FOUND) {
+        String newUrl = http.getLocation();
+        Serial.print("Redirigiendo a: ");
+        Serial.println(newUrl);
+
+        http.end(); // Termina la conexión anterior
+        http.begin(newUrl); // Nueva petición a la URL redirigida
+        httpCode = http.GET(); // Ejecuta GET de nuevo
+      }
+
+      if (httpCode == HTTP_CODE_OK) {
+
+        digitalWrite(ledPin, HIGH);
+        Serial.println("✅ 200 OK → LED ENCENDIDO");
+      } else {
+        digitalWrite(ledPin, LOW);
+        Serial.printf("⚠️ Código %d, LED apagado\n", httpCode);
+      }
+    } else {
+      Serial.printf("Error en conexión: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+    delay(1000);
+
+    digitalWrite(ledPin, LOW);
+  }
+  delay(5000);
+}
