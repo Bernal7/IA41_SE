1486617638c6ed6e9be658d7ca66fed7a78635b3
Angel <230223825+dotAngel@users.noreply.github.com>
2025-11-30 19:15:02 -0700

    Angel's Aport

--- LAB_5.ino
+++ LAB_5.ino
@@ -6,3 +6,10 @@
 #define DHTTYPE DHT11     // Cambia a DHT22 si usas ese sensor
 
 DHT dht(DHTPIN, DHTTYPE);
+
+// Dirección típica del LCD: 0x27 o 0x3F
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
